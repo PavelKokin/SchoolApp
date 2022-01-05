f054d3cecd62bb8e26caaeeef886c33d985ef13a
Pavel Kokin <pavelkok.nn@gmail.com>
2022-01-05 08:45:33 +0300

    Create Solutions

---
+++ SchoolApp/SchoolApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+
+        }
+    }
+}
