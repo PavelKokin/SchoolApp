fd1e759f07a2b5f0bfa22b96d0a5534bd7142e0b
Pavel Kokin <pavelkok.nn@gmail.com>
2022-01-05 10:42:24 +0300

    Add fuction registration students

--- SchoolApp/SchoolApp/Program.cs
+++ SchoolApp/SchoolApp/Program.cs
@@ -27,6 +27,26 @@ namespace SchoolApp
             Name = name;
             Students = new List<Student>();
         }
+        public void PrintStudents()
+        {
+            if (Students.Count == 0)
+            {
+                Console.WriteLine($"В школе \"{Name}\" пока нет учеников!");
+            }
+            else
+            {
+                Console.WriteLine("{0, -20} {1, -20}", "Имя", "Фамилия");
+                for (int i = 0; i < Students.Count; i++)
+                {
+                    Console.WriteLine("{0, -20} {1, -20}", Students[i].Name, Students[i].Surname);
+                }
+            }
+        }
+        public void AddNewStudent(Student student)
+        {
+            Students.Add(student);
+            Console.WriteLine($"В школу \"{Name}\" успешно добавлен ученик {student.Name}.");
+        }
     }
     internal class Program
     {
@@ -36,6 +56,26 @@ namespace SchoolApp
             string schoolName = Console.ReadLine();
             School school = new School(schoolName);
             Console.WriteLine($"Школа \"{school.Name}\" успешно создана!");
+            while (true)
+            {
+                Console.WriteLine($"Отобразить список учеников школы \"{school.Name}\"? Введите да или нет:");
+                string userAnswer = Console.ReadLine();
+                if (userAnswer == "да")
+                {
+                    school.PrintStudents();
+                }
+                Console.WriteLine($"Хотите добавить ученика в школу \"{school.Name}\"? Введите да или нет:");
+                userAnswer = Console.ReadLine();
+                if (userAnswer == "да")
+                {
+                    Console.WriteLine("Введите имя ученика:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Введите фамилию ученика:");
+                    string surname = Console.ReadLine();
+                    Student newStudent = new Student(name, surname);
+                    school.AddNewStudent(newStudent);
+                }
+            }
         }
     }
 }
