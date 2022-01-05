1d707ae4f1f9b1f306ff9cab7bde6fcd033623ef
Pavel Kokin <pavelkok.nn@gmail.com>
2022-01-05 09:14:10 +0300

    Create base class

--- SchoolApp/SchoolApp/Program.cs
+++ SchoolApp/SchoolApp/Program.cs
@@ -6,13 +6,36 @@ using System.Threading.Tasks;
 
 namespace SchoolApp
 {
+    public class Student
+    {
+        public string Name;
+        public string Surname;
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+    public class School
+    {
+        public string Name;
+        public List<Student> Students;
+
+        public School(string name)
+        {
+            Name = name;
+            Students = new List<Student>();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-
-
+            Console.WriteLine("Добро пожаловать в панель управления! Пожалуйста, введите название вашей школы:");
+            string schoolName = Console.ReadLine();
+            School school = new School(schoolName);
+            Console.WriteLine($"Школа \"{school.Name}\" успешно создана!");
         }
     }
 }
